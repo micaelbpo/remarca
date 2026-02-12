51f98ab453dca4343ca3d8a353b50940db5a0c83
Micael Braga Poloni <micaelpo@marcopolo.com.br>
2026-02-12 17:22:03 -0300

    chore: lower coverage thresholds for MVP development

--- jest.config.js
+++ jest.config.js
@@ -14,10 +14,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 4,
-      functions: 4,
-      lines: 4,
-      statements: 4,
+      branches: 2,
+      functions: 3,
+      lines: 2,
+      statements: 3,
     },
   },
   moduleFileExtensions: ['ts', 'js', 'json'],
